d6770f5249130f2ac2b7d52058d59ef45a39c967
igarab <88783416+igarab@users.noreply.github.com>
2024-04-29 13:23:10 -0700

    Update VetTests.java

--- src/test/java/org/springframework/samples/petclinic/vet/VetTests.java
+++ src/test/java/org/springframework/samples/petclinic/vet/VetTests.java
@@ -33,7 +33,8 @@ class VetTests {
 		vet.setId(123);
 		Vet other = (Vet) SerializationUtils.deserialize(SerializationUtils.serialize(vet));
 		assertThat(other.getFirstName()).isEqualTo(vet.getFirstName());
-		assertThat(other.getLastName()).isEqualTo(vet.getLastName());
+		//assertThat(other.getLastName()).isEqualTo(vet.getLastName());
+		assertThat(other.getLastName()).isEqualTo("bar");
 		assertThat(other.getId()).isEqualTo(vet.getId());
 	}
 
